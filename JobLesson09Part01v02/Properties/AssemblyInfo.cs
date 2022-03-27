1aced458aa149d1c1c047940c7dd1d3f6da9e866
Safroniv <safroniv@mail.ru>
2022-03-27 23:45:32 +0300

    Готовность 100% (ready 100%) File Manager + redact info & version.

--- JobLesson09Part01v02/Properties/AssemblyInfo.cs
+++ JobLesson09Part01v02/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.8.5.5")]
+[assembly: AssemblyVersion("3.8.6.5")]
 [assembly: AssemblyFileVersion("2.6.1.1")]
